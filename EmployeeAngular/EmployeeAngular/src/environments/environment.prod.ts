bcf5a357149ded14796c50be9e513a10bc5a59a8
Michael George Fernandes <michaelfernandes23@outlook.com>
2021-12-08 19:34:44 +0400

    Updated environment key

--- EmployeeAngular/EmployeeAngular/src/environments/environment.prod.ts
+++ EmployeeAngular/EmployeeAngular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:62769/'  
+  baseUrl: 'http://localhost:44294/'  
 };
--- EmployeeAngular/EmployeeAngular/src/environments/environment.ts
+++ EmployeeAngular/EmployeeAngular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {  
   production: false,  
-  baseUrl: 'http://localhost:62769/'  
+  baseUrl: 'http://localhost:44294/'  
 };
 
 /*
